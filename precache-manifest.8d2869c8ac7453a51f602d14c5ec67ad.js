dd2131518e3dda4b93171cfcf81ef939f8f8ccd2
Joe <joeschwehr@gmail.com>
2019-10-24 17:46:03 -0700

    Updates

---
+++ precache-manifest.8d2869c8ac7453a51f602d14c5ec67ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd39598a3322c50cef6ff4168e58c279",
+    "url": "/imagedetector/index.html"
+  },
+  {
+    "revision": "87f70d3eb7d684f0e206",
+    "url": "/imagedetector/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "3760fa210c1dcbc2aeff",
+    "url": "/imagedetector/static/css/main.0196e960.chunk.css"
+  },
+  {
+    "revision": "87f70d3eb7d684f0e206",
+    "url": "/imagedetector/static/js/2.a2685bff.chunk.js"
+  },
+  {
+    "revision": "3760fa210c1dcbc2aeff",
+    "url": "/imagedetector/static/js/main.fbf1561a.chunk.js"
+  },
+  {
+    "revision": "2fb5a25138da3c5db64e",
+    "url": "/imagedetector/static/js/runtime-main.f8ba2527.js"
+  }
+]);
